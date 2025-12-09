440f1e418bda290abc3a0f9fc09e6dde52ebd9fc
SORAAAAAAAAA <rcbriones95@gmail.com>
2025-12-09 11:42:57 +0800

    Fixed Data from backend

--- docker-be/backend/src/api/auth/auth.service.ts
+++ docker-be/backend/src/api/auth/auth.service.ts
@@ -7,19 +7,19 @@ const regiterUser = async (
     password: string
 ) => {
 
-    const userExists = await POOL.query('SELECT id FROM users WHERE email = $1', [email]);
+    const user = await POOL.query('SELECT * FROM users WHERE email = $1', [email]);
 
-    if (userExists.rowCount != null && userExists.rowCount > 0) {
+    if (user.rowCount != null && user.rowCount > 0) {
         throw { message: 'User with this email already exists', code: 'EMAIL_EXISTS' };
     }
 
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await POOL.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id', [name, email, hashedPassword]);
+    const newUser = await POOL.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, hashedPassword]);
 
     // return user without password
-    const { password: _pwd, ...userWithoutPassword } = user.rows[0];
+    const { password: _pwd, ...userWithoutPassword } = newUser.rows[0];
 
     return userWithoutPassword;
 }
